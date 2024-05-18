508984ec1fc56f125e4fa3b337cabe2ce4ce4c3a
Рябов Георгий Дмитриевич <gdryabov@edu.hse.ru>
2024-05-18 20:27:58 +0300

    chenge for pr

--- test_main.cpp
+++ test_main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-TEST(PARSE, head) { std::cout << "Wtf\n"; }
+TEST(PARSE, head) { EXPECT_TRUE(true); }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
