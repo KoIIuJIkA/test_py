ba809d6771d27f44addccd705c3462c352d2d7c8
Рябов Георгий Дмитриевич <gdryabov@edu.hse.ru>
2024-05-18 20:23:56 +0300

    prepare for PR

--- test_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(PARSE, head) { std::cout << "Wtf\n"; }
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();
-}
