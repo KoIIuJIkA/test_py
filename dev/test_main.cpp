f8ad432ed36035d81553676851d178195c86dad1
Рябов Георгий Дмитриевич <gdryabov@edu.hse.ru>
2024-05-18 22:15:56 +0300

    add test

--- dev/test_main.cpp
+++ dev/test_main.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
 
-TEST(PARSE, head) { EXPECT_TRUE(true); }
+#include "club.hpp"
+
+TEST(PARSE, head) {
+  std::ifstream file("/Users/georgryabov/Desktop/test/dev/data/testHead.txt");
+  Club *club = static_cast<Club *>(::operator new(sizeof(Club)));
+  check(parseHead(&file, club), "test parseHead");
+
+  bool flag = true;
+  // 3 tables + 1 table for fast access.
+  flag = flag && club->tables_.size() == 4;
+  flag = flag && club->open_ == static_cast<std::pair<uint16_t, uint16_t>>(
+                                    std::make_pair(9, 0));
+  flag = flag && club->close_ == static_cast<std::pair<uint16_t, uint16_t>>(
+                                     std::make_pair(19, 0));
+  flag = flag && club->price_ == 10;
+
+  delete club;
+  EXPECT_TRUE(flag);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
